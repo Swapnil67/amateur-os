8e7b2c122b768893f86d4f62aa8b1decd237b1d1
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2025-04-10 23:16:34 +0530

    Refactoring

--- src/asm/kernel.asm
+++ src/asm/kernel.asm
@@ -136,23 +136,10 @@ halt_cpu:
   ;; * ---------------------------------------------------------------
 
   ;; * ---------------------------------------------------------------
-  ;; * Print String
+  ;; * Include Files
   ;; * ---------------------------------------------------------------
-print_string: 
-	mov ah, 0x0e	        ; * BIOS int 10h/ah=0x0e, teletype output
-	mov bh, 0x00	        ; * page number
-  mov bl, 0x01          ; * color 
 
-print_char: 
-  mov al, [si]        ; * Move character value at address in si into al
-  cmp al, 0                   
-  je end_print        ; * jmp if equal (al = 0) to halt label
-  int 0x10            ; * Print character in al
-  add si, 1           ; * move 1 byte forward/ get next character
-  jmp print_char
-
-end_print:
-  ret
+  %include "./src/print/print_string.asm"
 
   ;; * ---------------------------------------------------------------
   ;; * Variables
--- src/print/print_string.asm
+++ src/print/print_string.asm
@@ -1,21 +1,23 @@
-	;; *
-	;; * Prints character string in BX register
-	;; *
-	
-print_string:
-	pusha		; * Store all Register value onto the stack
-	;; Tele-type output
-	mov ah, 0x0e	; * int 10/ ah 0x0e BIOS teletype output
-	
-print_char:	
-	mov al, [bx]	; * mov character value at address in bx int al
-	cmp al, 0
-	je .print_string_end
-	int 0x10	; * print character in al
-	add bx, 1	; * move 1 byte forward/ get to next cursor
-	jmp print_char	; * loop
-	
-.print_string_end:
+;; * ---------------------------------------------------------------
+;; * Print String in SI register
+;; * ---------------------------------------------------------------
+
+print_string: 
+	pusha
+	mov ah, 0x0e	        ; * BIOS int 10h/ah=0x0e, teletype output
+	mov bh, 0x00	        ; * page number
+  mov bl, 0x01          ; * color 
+
+print_char: 
+  ; mov al, [si]        ; * Move character value at address in si into al
+	lodsb               ; * Move byte at si into alx
+  cmp al, 0                   
+  je end_print        ; * jmp if equal (al = 0) to halt label
+  int 0x10            ; * Print character in al
+  ; add si, 1           ; * move 1 byte forward/ get next character
+  jmp print_char
+
+end_print:
 	popa
-	ret
+  ret
 
